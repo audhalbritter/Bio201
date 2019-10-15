8c95bff7439091a6b61ecee42b4d39caf412802e
audhalbritter <audhalbritter.com>
2019-10-15 11:36:58 +0200

    initial commit

---
+++ CommunityDynamics.R
@@ -0,0 +1,19 @@
+########################################
+### Chapter 18 - Community Dynamics ###
+   ### Community change over time ###
+########################################
+
+# Load libraries
+library("tidyverse")
+library("readxl")
+
+dat <- read_excel(path = "Heathland.xlsx")
+
+# 1) Plot species over time
+
+
+
+
+
+# 2) Regeneration strategy
+
